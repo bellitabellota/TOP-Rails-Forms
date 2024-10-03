dd130f7f112138ef8a6e68845ea0e69dbc24fe6f
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-03 11:09:02 +0100

    Complete the basic form in  #new view file and add #create method in UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(permitted_user_param)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def permitted_user_param
+    params.require(:user).permit(:username, :email, :password)
   end
 end
