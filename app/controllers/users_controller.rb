58da43c9cab9422e76cb68977fa7e61790ad4b40
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-03 18:12:22 +0100

    Add #edit method and view displaying errors if update fails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(permitted_user_param)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
     @user = User.new()
   end
